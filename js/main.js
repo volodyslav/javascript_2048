5f923b3ba2af9061d84984e27a27e195e5c9a673
Vladyslav <vladver1211@gmail.com>
2024-08-31 16:27:38 +0300

    change: tiles movement with while loop

--- js/main.js
+++ js/main.js
@@ -18,23 +18,23 @@ let tilesArray = [
 const colorValue = {
     0: "rgb(150, 148, 148)",
     2: "rgb(218, 211, 211)",
-    4: "rgb(218, 211, 211)",
-    8: "rgb(218, 211, 211)",
-    16: "rgb(218, 211, 211)",
-    32: "rgb(218, 211, 211)",
-    64: "rgb(218, 211, 211)",
-    128: "rgb(218, 211, 211)",
+    4: "rgb(201, 192, 169)",
+    8: "rgb(194, 143, 23)",
+    16: "rgb(217, 84, 17)",
+    32: "rgb(181, 61, 0)",
+    64: "rgb(148, 51, 1)",
+    128: "rgb(224, 221, 22)",
 }
 
 // Text colors
 const color = {
     2: "black",
     4: "black",
-    8: "black",
-    16: "black",
-    32: "black",
-    64: "black",
-    128: "black",
+    8: "white",
+    16: "white",
+    32: "white",
+    64: "white",
+    128: "white",
 }
 
 function drawTiles(row, col){
@@ -81,15 +81,20 @@ function generateTiles(){
 function moveTilesRight(){
     for (let row = 3; row >= 0; row--){
         for (let col = 3; col >= 0; col--){
-            if (col !== 0 && (tilesArray[row][col] === tilesArray[row][col-1])){
-                tilesArray[row][col] *= 2;
-                tilesArray[row][col-1] = 0;
-                drawTiles(row, col);
-            }
-            if (col !== 0 && tilesArray[row][col] === 0 && tilesArray[row][col - 1] !== 0){
-                tilesArray[row][col] = tilesArray[row][col - 1];
-                tilesArray[row][col - 1] = 0;
-                drawTiles(row, col);
+            let prevColumn = col // Get the column
+            // Start to iter from 3 to 0 and then combine or move the tile
+            while (prevColumn !== 0 && (tilesArray[row][prevColumn - 1] === 0 || tilesArray[row][prevColumn - 1] !== 0)){
+                prevColumn--;
+                if(tilesArray[row][col] === tilesArray[row][prevColumn]){
+                    tilesArray[row][col] *= 2;
+                    tilesArray[row][prevColumn] = 0;
+                    drawTiles(row, col);
+                }
+                if (tilesArray[row][col] === 0 && tilesArray[row][prevColumn] !== 0){
+                    tilesArray[row][col] = tilesArray[row][prevColumn];
+                    tilesArray[row][prevColumn] = 0;
+                    drawTiles(row, col);
+                }
             }
         }
     }
@@ -98,46 +103,64 @@ function moveTilesRight(){
 function moveTilesLeft(){
     for (let row = 0; row < tilesColumn; row++){
         for (let col = 0; col < tilesColumn; col++){
-            if (col !== 3 && (tilesArray[row][col] === tilesArray[row][col + 1])){
-                tilesArray[row][col] *= 2;
-                tilesArray[row][col+1] = 0;
-                drawTiles(row, col);
-            }if (col !== 3 && tilesArray[row][col] === 0 && tilesArray[row][col + 1] !== 0){
-                tilesArray[row][col] = tilesArray[row][col + 1];
-                tilesArray[row][col + 1] = 0;
-                drawTiles(row, col);
+            let prevColumn = col // Get the previuos column
+            // Start to iter from 0 to 3 and then combine or move the tile
+            while (prevColumn !== 3 && (tilesArray[row][prevColumn + 1] === 0 || tilesArray[row][prevColumn + 1] !== 0)){
+                prevColumn++;
+                if(tilesArray[row][col] === tilesArray[row][prevColumn]){
+                    tilesArray[row][col] *= 2;
+                    tilesArray[row][prevColumn] = 0;
+                    drawTiles(row, col);
+                }
+                if (tilesArray[row][col] === 0 && tilesArray[row][prevColumn] !== 0){
+                    tilesArray[row][col] = tilesArray[row][prevColumn];
+                    tilesArray[row][prevColumn] = 0;
+                    drawTiles(row, col);
+                }
             }
         }
     }
 }
 
 function moveTilesDown(){
-    for (let row = 0; row < tilesColumn; row++){
-        for (let col = 0; col < tilesColumn; col++){
-            if (row !== 3 && (tilesArray[row][col] === tilesArray[row+1][col])){
-                tilesArray[row + 1][col] *= 2;
-                tilesArray[row][col] = 0;
-                drawTiles(row, col);
-            }if (row !== 3 && tilesArray[row + 1][col] === 0 && tilesArray[row][col] !== 0){
-                tilesArray[row + 1][col] = tilesArray[row][col];
-                tilesArray[row][col] = 0;
-                drawTiles(row, col);
+    for (let row = 3; row >= 0; row--){
+        for (let col = 3; col >= 0; col--){
+            let prevRow = row  // Get the row
+            // Start to iter from 3 to 0 and then combine or move the tile
+            while (prevRow !== 0 && (tilesArray[prevRow - 1][col] === 0 || tilesArray[prevRow - 1][col] !== 0)){
+                prevRow--;
+                if(tilesArray[row][col] === tilesArray[prevRow][col]){
+                    tilesArray[row][col] *= 2;
+                    tilesArray[prevRow][col] = 0;
+                    drawTiles(row, col);
+                }
+                if (tilesArray[row][col] === 0 && tilesArray[prevRow][col] !== 0){
+                    tilesArray[row][col] = tilesArray[prevRow][col];
+                    tilesArray[prevRow][col] = 0;
+                    drawTiles(row, col);
+                }
             }
         }
     }
 }
 
 function moveTilesUp(){
-    for (let row = 3; row >= 0; row--){
-        for (let col = 3; col >= 0; col--){
-            if (row !== 0 && (tilesArray[row][col] === tilesArray[row-1][col])){
-                tilesArray[row - 1][col] *= 2;
-                tilesArray[row][col] = 0;
-                drawTiles(row, col);
-            }if (row !== 0 && tilesArray[row - 1][col] === 0 && tilesArray[row][col] !== 0){
-                tilesArray[row - 1][col] = tilesArray[row][col];
-                tilesArray[row][col] = 0;
-                drawTiles(row, col);
+    for (let row = 0; row < tilesColumn; row++){
+        for (let col = 0; col < tilesColumn; col++){
+            let prevRow = row  // Get the row
+            // Start to iter from 3 to 0 and then combine or move the tile
+            while (prevRow !== 3 && (tilesArray[prevRow + 1][col] === 0 || tilesArray[prevRow + 1][col] !== 0)){
+                prevRow++;
+                if(tilesArray[row][col] === tilesArray[prevRow][col]){
+                    tilesArray[row][col] *= 2;
+                    tilesArray[prevRow][col] = 0;
+                    drawTiles(row, col);
+                }
+                if (tilesArray[row][col] === 0 && tilesArray[prevRow][col] !== 0){
+                    tilesArray[row][col] = tilesArray[prevRow][col];
+                    tilesArray[prevRow][col] = 0;
+                    drawTiles(row, col);
+                }
             }
         }
     }
@@ -159,7 +182,9 @@ function handleKeyboards(){
         } else if (event.key === "ArrowRight" || event.key === "d") {
             moveTilesRight();
         }
+        generateInitialTiles(); // Generate initial tiles 2
         generateTiles();
+        
     }); 
 }
 
